c1ba340e74d023acba262eaf6c8f3067e1b25685
mollexmax048 <mollexmax048@gmail.com>
2024-10-02 17:55:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,35 +1,35 @@
 //base by DGXeon
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @GlobalTechInfo
-//Telegram: https://t.me/GlobalTechInc
-//GitHub: @GlobalTechInfo
-//WhatsApp: https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@GlobalTechInfo
+//YouTube: @KEYTWOTZ  ["https://youtube.com/channel/UC1CX8405F3G0gFxbuQJT8Pg?si=JqwhcipFDLfSiBW4"]
+//Telegram: ["https://t.me/computersciencecome","https://t.me/channelKEYTWOTV"]
+//GitHub: @mollexmax048
+//WhatsApp: https://chat.whatsapp.com/G01QWFkzkanKlK7TTrOzxe
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@keytwotz?si=Ql8d98w0rfW9mBPS
 
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "14143066181"
-global.ownername = "MoeVippeer"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.ownernomer = "255685153360"
+global.ownername = "KEYTWO TV"
+global.ytname = "YT: Keytwotz"
+global.socialm = "Facebook: https://facebook.com/groups/487156432293620/"
+global.location = "Tanzania, Kinondoni, Dares'alaam"
 
-global.ownernumber = '14143066181'  //creator number
-global.ownername = 'Moe vippeer' //owner name
+global.ownernumber = '255685153360'  //creator number
+global.ownername = 'KEY TWO TV' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'Keytwotz\n\nContact: +255685153360'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VaEzltkCcW4sEJ3w0b2w'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
